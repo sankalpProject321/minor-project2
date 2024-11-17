ec8aca2f5a98370532b973a09217b77fd119c74c
Sankalp-arya <142680402+sankalpProject321@users.noreply.github.com>
2024-11-17 14:32:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: ["https://minor-project-frontend-delta.vercel.app"],
+    origin: ["https://minor-project-frontend-two.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })
